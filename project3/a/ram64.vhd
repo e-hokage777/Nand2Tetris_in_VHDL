4b5b8e229837b372856a154eb78f95abe4085da8
e-hokage777 <e.hokage777@gmail.com>
2023-09-05 23:23:10 +0000

    implements ram64 chip

--- project3/a/ram64.vhd
+++ project3/a/ram64.vhd
@@ -1,17 +1,18 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
-ENTITY ram8 IS
+ENTITY ram64 IS
     PORT (
         input : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-        address : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+        address : IN STD_LOGIC_VECTOR(5 DOWNTO 0);
         load : IN STD_LOGIC;
         clk : IN STD_LOGIC;
         output : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
     );
-END ENTITY ram8;
+END ENTITY ram64;
 
-ARCHITECTURE ram8_arch OF ram8 IS
+ARCHITECTURE ram64_arch OF ram64 IS
+
     -- load signals for each register
     SIGNAL load_0 : STD_LOGIC;
     SIGNAL load_1 : STD_LOGIC;
@@ -21,6 +22,7 @@ ARCHITECTURE ram8_arch OF ram8 IS
     SIGNAL load_5 : STD_LOGIC;
     SIGNAL load_6 : STD_LOGIC;
     SIGNAL load_7 : STD_LOGIC;
+
     -- output signals for each register
     SIGNAL output_0 : STD_LOGIC_VECTOR(15 DOWNTO 0);
     SIGNAL output_1 : STD_LOGIC_VECTOR(15 DOWNTO 0);
@@ -35,7 +37,7 @@ BEGIN
     load_dmux : ENTITY work.dmux8way(dmux8way_arch)
         PORT MAP(
             input => load,
-            sel => address,
+            sel => address(5 DOWNTO 3),
             out0 => load_0,
             out1 => load_1,
             out2 => load_2,
@@ -46,23 +48,23 @@ BEGIN
             out7 => load_7
         );
 
-    -- getting the outputs from the registers
-    register_0 : ENTITY work.regist(regist_arch)
-        PORT MAP (input => input, load => load_0, clk => clk, output => output_0);
-    register_1 : ENTITY work.regist(regist_arch)
-        PORT MAP (input => input, load => load_1, clk => clk, output => output_1);
-    register_2 : ENTITY work.regist(regist_arch)
-        PORT MAP (input => input, load => load_2, clk => clk, output => output_2);
-    register_3 : ENTITY work.regist(regist_arch)
-        PORT MAP (input => input, load => load_3, clk => clk, output => output_3);
-    register_4 : ENTITY work.regist(regist_arch)
-        PORT MAP (input => input, load => load_4, clk => clk, output => output_4);
-    register_5 : ENTITY work.regist(regist_arch)
-        PORT MAP (input => input, load => load_5, clk => clk, output => output_5);
-    register_6 : ENTITY work.regist(regist_arch)
-        PORT MAP (input => input, load => load_6, clk => clk, output => output_6);
-    register_7 : ENTITY work.regist(regist_arch)
-        PORT MAP (input => input, load => load_7, clk => clk, output => output_7);
+    -- getting the outputs from the ram8 chips
+    ram8_0 : ENTITY work.ram8(ram8_arch)
+        PORT MAP(input => input, address => address(2 DOWNTO 0), load => load_0, clk => clk, output => output_0);
+    ram8_1 : ENTITY work.ram8(ram8_arch)
+        PORT MAP(input => input, address => address(2 DOWNTO 0), load => load_1, clk => clk, output => output_1);
+    ram8_2 : ENTITY work.ram8(ram8_arch)
+        PORT MAP(input => input, address => address(2 DOWNTO 0), load => load_2, clk => clk, output => output_2);
+    ram8_3 : ENTITY work.ram8(ram8_arch)
+        PORT MAP(input => input, address => address(2 DOWNTO 0), load => load_3, clk => clk, output => output_3);
+    ram8_4 : ENTITY work.ram8(ram8_arch)
+        PORT MAP(input => input, address => address(2 DOWNTO 0), load => load_4, clk => clk, output => output_4);
+    ram8_5 : ENTITY work.ram8(ram8_arch)
+        PORT MAP(input => input, address => address(2 DOWNTO 0), load => load_5, clk => clk, output => output_5);
+    ram8_6 : ENTITY work.ram8(ram8_arch)
+        PORT MAP(input => input, address => address(2 DOWNTO 0), load => load_6, clk => clk, output => output_6);
+    ram8_7 : ENTITY work.ram8(ram8_arch)
+        PORT MAP(input => input, address => address(2 DOWNTO 0), load => load_7, clk => clk, output => output_7);
 
     -- selecting the output
     mux8way16_comp : ENTITY work.mux8way16(mux8way16_arch)
@@ -75,8 +77,7 @@ BEGIN
             f => output_5,
             g => output_6,
             h => output_7,
-            sel => address,
+            sel => address(5 DOWNTO 3),
             output => output
         );
-
-END ARCHITECTURE ram8_arch;
+END ARCHITECTURE ram64_arch;
